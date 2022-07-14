fb38b9c723ae5eda0c2e36924b67a048166e0030
It's Lex <lex@crema.us>
2022-07-14 17:27:10 -0500

    Update App file

--- App.tsx
+++ App.tsx
@@ -1,12 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native'
+import { NativeBaseProvider, Box } from 'native-base'
+import {NavigationContainer, DefaultTheme} from '@react-navigation/native'
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs'
+import { CompletedScreen } from './src/screens/CompletedScreen';
+import { TodoScreen } from './src/screens/TodoScreen';
+
+const MyTheme = {
+  dark: false,
+  colors: {
+    primary: '#f98284',
+    background: '#FFFAFA',
+    card: 'white',
+    text: 'white',
+    border: 'rgb(199, 199, 204)',
+    notification: 'rgb(255, 69, 58)',
+    navigator: "green"
+  },
+};
+
+
+const Tab = createMaterialTopTabNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello There !</Text>
-      <StatusBar style="auto" />
-    </View>
+   <NativeBaseProvider>
+        <View style={styles.box}></View>
+        <NavigationContainer theme={MyTheme}>
+        <Tab.Navigator
+          screenOptions={{ tabBarStyle: {backgroundColor:"#28282E"}}}
+        >
+        <Tab.Screen name="Todo" component={TodoScreen} />
+        <Tab.Screen name="Completed" component={CompletedScreen} />
+      </Tab.Navigator>
+        </NavigationContainer>
+   </NativeBaseProvider>
   );
 }
 
@@ -16,4 +44,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  box:{
+   height:25
+  }
 });
