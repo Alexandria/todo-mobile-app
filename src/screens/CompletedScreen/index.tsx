41eb6d671fd5065dfc191c1216a91c96c8ebce36
It's Lex <lex@crema.us>
2022-07-14 17:26:50 -0500

    Add CompletedScreen

---
+++ src/screens/CompletedScreen/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+export const CompletedScreen = () =>{
+    return <View style={styles.container}>
+      <Text> Completed </Text>
+    </View>
+  }
+
+  const styles = StyleSheet.create({
+    container:{
+        paddingHorizontal:20
+    }
+})
